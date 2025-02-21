ad2f2c6433b56c458e90b503a0fd43e096063e96
Adarsh-s452 <adarshappuadhi@.com>
2025-02-21 13:04:35 +0530

    third

--- server.js
+++ server.js
@@ -26,3 +26,4 @@ app.get('/movies/getMovieById/:id', getMovieById);  // Use the getMovieById cont
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
